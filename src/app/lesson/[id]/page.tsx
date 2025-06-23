36768da563939e0e3a1cd2ddeaeb86ebd1f31ddf
dika muhammad <dikaamuhammad90@gmail.com>
2025-06-24 00:49:10 +0800

    lesson page error

--- src/app/lesson/[id]/page.tsx
+++ src/app/lesson/[id]/page.tsx
@@ -3,12 +3,12 @@ import { allLessons } from "@/lib/lessons";
 import { notFound } from "next/navigation";
 import LessonView from "./lesson-view";
 
-export default async function LessonPage(props: any): Promise<JSX.Element> {
-  const lessonData = allLessons[props.params.id];
+export default async function LessonPage({ params }: { params: Record<string, string> }): Promise<JSX.Element> {
+  const lessonData = allLessons[params.id];
 
   if (!lessonData) {
     notFound();
   }
 
-  return <LessonView lessonData={lessonData} courseId={props.params.id} />;
+  return <LessonView lessonData={lessonData} courseId={params.id} />;
 }
